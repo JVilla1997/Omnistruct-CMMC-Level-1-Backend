7af165197f78e60af1dd1bb487fc97df5cfde49f
JVilla1997 <jorge.villavicencio.013@gmail.com>
2022-11-08 14:13:34 -0800

    update environment

---
+++ src/main/java/com/altf4omni/omnicmmc/entity/Person.java
@@ -0,0 +1,23 @@
+//package com.altf4omni.omnicmmc.entity;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+////
+//@Entity
+//@Table(name = "Person")
+//
+//public class Person {
+//
+//    @Id
+//    @Column(name = "LICENSE")
+//    private String license;
+//
+//    @Column(name = "FIRSTNAME")
+//    private String firstName;
+//
+//    @Column(name = "LASTNAME")
+//    private String lastName;
+//}
