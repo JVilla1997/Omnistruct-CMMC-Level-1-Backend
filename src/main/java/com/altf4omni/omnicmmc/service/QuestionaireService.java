ca44a7608fb0a06eb381a76b7fd008fc1dac716d
JVilla1997 <jorge.villavicencio.013@gmail.com>
2023-02-14 17:41:21 -0800

    updated entites in the data base to match the new EER diagram

---
+++ src/main/java/com/altf4omni/omnicmmc/service/QuestionaireService.java
@@ -0,0 +1,28 @@
+package com.altf4omni.omnicmmc.service;
+
+import com.altf4omni.omnicmmc.dto.QuestionCreationRequest;
+import com.altf4omni.omnicmmc.entity.Question;
+import com.altf4omni.omnicmmc.entity.Questions;
+import com.altf4omni.omnicmmc.repository.QuestionaireRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * This java class will have the logic for the question and questionAnswer
+ */
+@Service
+public class QuestionaireService {
+    private final QuestionaireRepository questionaireRepository;
+
+    public QuestionaireService(QuestionaireRepository questionaireRepository) {
+        this.questionaireRepository = questionaireRepository;
+    }
+    public String createQuestion(QuestionCreationRequest request) {
+        //var question asks for questionName and questionAnswer from dto/QuestionCreationRequest and creates an entity
+        //and of type entity/Questionaire
+        var question = Questions.builder()
+                .prompt(request.getQuestionName())
+                .build();
+        questionaireRepository.save(question);
+        return "Question created successfully!";
+    }
+}
