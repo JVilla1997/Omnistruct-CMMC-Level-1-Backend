cf76b5204717eebdc889dd7ee47c3fac22328fec
AnBat07 <anitabatishev@gmail.com>
2023-02-15 18:49:25 -0800

    Refactored. Added a .yml file

--- src/main/java/com/altf4omni/omnicmmc/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.altf4omni.omnicmmc.repository;
-//
-//import com.altf4omni.omnicmmc.entity.Person;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface PersonRepository extends JpaRepository<Person, String> {
-//
-//    Person findPersonByFirstName(String firstName);
-//}
