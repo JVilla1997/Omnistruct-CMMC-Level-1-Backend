d249893e28c6702ee8c52a876d530a6bff4f099a
JVilla1997 <jorge.villavicencio.013@gmail.com>
2023-03-01 14:47:54 -0800

    created the hierearchy for questionnaireService

--- src/main/java/com/altf4omni/omnicmmc/controller/QuestionaireController.java
+++ src/main/java/com/altf4omni/omnicmmc/controller/QuestionaireController.java
@@ -1,11 +1,11 @@
 package com.altf4omni.omnicmmc.controller;
 
-import com.altf4omni.omnicmmc.dto.QuestionCreationRequest;
 import com.altf4omni.omnicmmc.dto.QuestionnaireResponse;
 import com.altf4omni.omnicmmc.service.QuestionaireService;
 import groovy.util.logging.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -15,14 +15,14 @@ public class QuestionaireController {
     public QuestionaireController(QuestionaireService questionaireService) {
         this.questionaireService = questionaireService;
     }
+
     /**
-     *
-     * @param
-     * @return
+     * Get a questionnaire.
+     * @return {@link QuestionnaireResponse}
      */
     @GetMapping("/questions")
     public ResponseEntity<QuestionnaireResponse> getQuestionnaire() {
-        var response = questionaireService.createQuestion(request);
+        var response = questionaireService.getQuestionnaire();
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/altf4omni/omnicmmc/dto/QuestionnaireResponse.java
+++ src/main/java/com/altf4omni/omnicmmc/dto/QuestionnaireResponse.java
@@ -1,7 +1,6 @@
 package com.altf4omni.omnicmmc.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +9,6 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class QuestionnaireResponse {
     private List<QuestionSectionDto> questionSectionDtos;
 }
