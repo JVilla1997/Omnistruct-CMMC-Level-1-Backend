e4872b84964342cb06fd4cf16700facf0bced20e
xchrissonx <cson@csus.edu>
2023-03-13 16:52:42 -0700

    Added UserDetailsService

---
+++ src/main/java/com/altf4omni/omnicmmc/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.altf4omni.omnicmmc.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException
+            {
+                User user = userRepository.getUserByUsername(username);
+
+                if(user == null)
+                {
+                    throw new UsernameNotFoundException("Could not find user");
+                }
+
+                return new MyUserDetails(user);
+            }
+}
