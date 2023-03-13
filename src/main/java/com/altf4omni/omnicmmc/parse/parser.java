47265a8408a40c5517531a5f2de2045dc852ce30
Danny Perez <danielperez2@csus.edu>
2023-03-13 16:39:26 -0700

    All JSON sections have been added

--- src/main/java/com/altf4omni/omnicmmc/parse/parser.java
+++ src/main/java/com/altf4omni/omnicmmc/parse/parser.java
@@ -1,4 +1,24 @@
 package com.altf4omni.omnicmmc.parse;
 
+import java.util.List;
+
 public class parser {
+    private String answer;
+    private String description;
+    private String prompt;
+    private String result;
+    private List <ExtendedQuestionAnswer> extendedQuestionAnswers;
+
+    public class ExtendedQuestionAnswer{
+        private String prompt;
+        private String description;
+        private String answer;
+        private String result;
+
+    }
+    public class QuestionAnswers{
+        private String sectionName;
+        private List<parser> answerList;
+
+    }
 }
