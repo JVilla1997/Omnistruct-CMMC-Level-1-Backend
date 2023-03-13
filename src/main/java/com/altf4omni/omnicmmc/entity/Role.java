6dd99575907ec12437bce71304dcd1484089aa7d
xchrissonx <cson@csus.edu>
2023-03-13 16:10:23 -0700

    Added entities (Role/User) for backend credentials

---
+++ src/main/java/com/altf4omni/omnicmmc/entity/Role.java
@@ -0,0 +1,28 @@
+package com.altf4omni.omnicmmc.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "ROLES")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    /*
+    public Integer getId(){
+        return id;
+    }
+    */
+}
---
+++ src/main/java/com/altf4omni/omnicmmc/entity/User.java
@@ -0,0 +1,37 @@
+package com.altf4omni.omnicmmc.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "USERS")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private boolean enabled;
+
+    private Set<Role> roles = new HashSet<>();
+
+    /*
+    public Long getID(){
+        return id;
+    }
+    */
+
+}
