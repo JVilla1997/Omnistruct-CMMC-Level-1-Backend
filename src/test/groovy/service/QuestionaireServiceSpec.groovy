017d24ec8f199ecbe691ed369ac2a9b14d164999
JVilla1997 <jorge.villavicencio.013@gmail.com>
2023-03-15 19:06:44 -0700

    CMMC-90 Create unit tests for getQuestionnaire

--- src/test/groovy/service/QuestionaireServiceSpec.groovy
+++ src/test/groovy/service/QuestionaireServiceSpec.groovy
@@ -1,33 +1,95 @@
 package service
 
-import com.altf4omni.omnicmmc.dto.QuestionCreationRequest
+import com.altf4omni.omnicmmc.dto.QuestionnaireResponse
+import com.altf4omni.omnicmmc.entity.ExtendedQuestion
 import com.altf4omni.omnicmmc.entity.Question
-import com.altf4omni.omnicmmc.repository.QuestionRepository
+import com.altf4omni.omnicmmc.entity.QuestionSection
+import com.altf4omni.omnicmmc.repository.QuestionSectionRepository
 import com.altf4omni.omnicmmc.service.QuestionaireService
 import spock.lang.Specification
 
 class QuestionaireServiceSpec extends Specification {
-    //No semi-colon, skrrr skrrr
-    //'Def' groovy is smart enough to figure out the type of variable, groovy is a good boy
-    def questionaireRepository = Mock(QuestionRepository)
-    //This is telling us that we are going to be testing over Questionaire service.
-    def sut = new QuestionaireService(questionaireRepository, extendedQuestionRepository, questionSectionRepository)
-    //This will be our method name
-    def "Is create question method actually creating a method? Sussy >_>"() {
-        given: "A question, duh"
-        def questionRequest =
-                new QuestionCreationRequest(questionName: "Why am I so Lazy??", questionAnswer: "Just git gud boii")
-        def questionEntity = Question.builder().prompt("Why am I so Lazy??").build()
-        when: "Create question is called"
-        def response = sut.createQuestion(questionRequest)
-        then: "The response sucks, but is correct"
-        //Making sure response is not null, funny according to Dani. Questionable
+    def questionaireSectionRepository = Mock(QuestionSectionRepository)
+    def sut = new QuestionaireService(questionaireSectionRepository)
+
+    def "getQuestionnaire is successful"() {
+        when: "getQuestionnaire is called"
+        def response = sut.getQuestionnaire()
+        then: "It should return a questionnaire with the right hierarchy"
         response
-        //We are expecting our response to be this static String, else we are screwed
-        response == "Question created successfully!"
-        and: "The expected sussyness occur"
-        1 * questionaireRepository.save(questionEntity)
-        // '_' means anything, meaning nothing else happens. After this there won't be any other call
+        response.sections.size() == 1
+        response.sections[0].sectionName == 'Hello World'
+        response.sections[0].sequenceNumber == 1
+        response.sections[0].questions.size() == 1
+        response.sections[0].questions[0].prompt == 'Hello World'
+        response.sections[0].questions[0].description == 'Bye World'
+        response.sections[0].questions[0].sequenceNumber == 1
+        response.sections[0].questions[0].flag
+        response.sections[0].questions[0].extendedQuestion.size() == 1
+        response.sections[0].questions[0].extendedQuestion[0].prompt == 'Hello World extended'
+        response.sections[0].questions[0].extendedQuestion[0].description == 'Bye World extended'
+        response.sections[0].questions[0].extendedQuestion[0].sequenceNumber == 1
+
+        and: "The expected interactions occur"
+        1 * questionaireSectionRepository.findAll() >> [new QuestionSection(1, 'Hello World',
+                1, [new Question(1, "Hello World", "Bye World", 1, Boolean.TRUE
+                , [new ExtendedQuestion(1, "Hello World extended", "Bye World extended", 1)])])]
         0 * _
     }
+        def "Get questionnaire errors if repository fails"() {
+            when: "getQuestionnaire is called"
+            def response = sut.getQuestionnaire()
+            then: "It should return a questionnaire with the right hierarchy"
+            response == new QuestionnaireResponse()
+
+            and: "The expected interactions occur"
+            1 * questionaireSectionRepository.findAll() >> []
+            0 * _
+    }
+
+    def "Should not return npe if a question does not have extended quesitons"() {
+        when: "getQuestionnaire is called"
+        def response = sut.getQuestionnaire()
+        then: "It should return a questionnaire with the right hierarchy"
+        response
+        response.sections.size() == 1
+        response.sections[0].sectionName == 'Hello World'
+        response.sections[0].sequenceNumber == 1
+        response.sections[0].questions.size() == 1
+        response.sections[0].questions[0].prompt == 'Hello World'
+        response.sections[0].questions[0].description == 'Bye World'
+        response.sections[0].questions[0].sequenceNumber == 1
+        response.sections[0].questions[0].flag
+        response.sections[0].questions[0].extendedQuestion.size() == 0
+        and: "No null pointer exception should be thrown"
+        noExceptionThrown()
+
+        and: "The expected interactions occur"
+        1 * questionaireSectionRepository.findAll() >> [
+                new QuestionSection(1, 'Hello World',
+                1, [
+                        new Question(1, "Hello World", "Bye World", 1, Boolean.TRUE, [])])
+                 ]
+        0 * _
+    }
+
+    def "Should not return npe if a section does not have questions"() {
+        when: "getQuestionnaire is called"
+        def response = sut.getQuestionnaire()
+        then: "It should return a questionnaire with the right hierarchy"
+        response
+        response.sections.size() == 1
+        response.sections[0].sectionName == 'Hello World'
+        response.sections[0].sequenceNumber == 1
+        response.sections[0].questions.size() == 0
+        and: "No null pointer exception should be thrown"
+        noExceptionThrown()
+
+        and: "The expected interactions occur"
+        1 * questionaireSectionRepository.findAll() >> [
+                new QuestionSection(1, 'Hello World', 1, [])
+        ]
+        0 * _
+    }
+
 }
