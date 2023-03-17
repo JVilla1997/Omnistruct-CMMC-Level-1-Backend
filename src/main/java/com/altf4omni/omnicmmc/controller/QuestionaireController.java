96789be76e1557a71b0601d690cd7df8abcce6fd
Danny Perez <danielperez2@csus.edu>
2023-03-17 02:53:06 -0700

    Made significant progess to the PDF generator

--- src/main/java/com/altf4omni/omnicmmc/controller/QuestionaireController.java
+++ src/main/java/com/altf4omni/omnicmmc/controller/QuestionaireController.java
@@ -1,10 +1,13 @@
 package com.altf4omni.omnicmmc.controller;
 
 import com.altf4omni.omnicmmc.dto.AnswerRequest;
+import com.altf4omni.omnicmmc.dto.ExtendedQuestionAnswerDto;
 import com.altf4omni.omnicmmc.dto.QuestionAnswerDto;
 import com.altf4omni.omnicmmc.dto.QuestionnaireResponse;
+import com.altf4omni.omnicmmc.enumeration.PassFailResult;
 import com.altf4omni.omnicmmc.service.QuestionaireService;
 import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.pdf.PdfWriter;
 import groovy.util.logging.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
 import java.io.ByteArrayOutputStream;
 
 @Slf4j
@@ -48,12 +52,36 @@ public class QuestionaireController {
 
         Paragraph SectionName = new Paragraph(answerRequest.getSectionName());
         Paragraph AnswerList = new Paragraph();
+        ExtendedQuestionAnswerDto extendedQuestionDto = answerRequest.getAnswerList().getExtendedQuestionDto();
 
-        document.add(SectionName);
+        QuestionAnswerDto questionAnswer = new QuestionAnswerDto();
+        PassFailResult result;
+
+        document.add((new Paragraph("Section: " + SectionName)));
         for(QuestionAnswerDto questionAnswerDto: answerRequest.getAnswerList()){
-            document.add(new Paragraph (questionAnswerDto.getAnswer()));
-            document.add(new Paragraph(questionAnswerDto.getDescription()));
+            document.add(new Paragraph ("Answer: " + questionAnswerDto.getAnswer()));
+            document.add(new Paragraph("Description: " + questionAnswerDto.getDescription()));
+            document.add(new Paragraph("Prompt: " + questionAnswerDto.getPrompt()));
+
+            result = questionAnswer.getResult();
+            if(result == PassFailResult.PASS){
+                Chunk chunk = new Chunk("Result: Pass");
+                chunk.setFont(FontFactory.getFont(FontFactory.HELVETICA, 12, Font.NORMAL, BaseColor.GREEN));
+                document.add(new Paragraph(chunk));
+            } else {
+                Chunk chunk = new Chunk("Result: Fail");
+                chunk.setFont(FontFactory.getFont(FontFactory.HELVETICA, 12, Font.NORMAL, BaseColor.RED));
+                document.add(new Paragraph(chunk));
+            }
         }
+
+        for(ExtendedQuestionAnswerDto extendedQuestionAnswerDto: questionAnswer.getExtendedQuestionAnswers()){
+            document.add(new Paragraph ("Answer: " + extendedQuestionAnswerDto.getAnswer()));
+
+
+        }
+
+
 
         document.close();
         byte[] pdfData = pdfOutput.toByteArray();
