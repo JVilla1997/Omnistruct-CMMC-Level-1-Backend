8dc06d84684c420dde92a91519a7306206c97ad9
AnBat07 <anitabatishev@gmail.com>
2023-03-31 17:04:04 -0700

    changed to security and pom

---
+++ src/main/java/com/altf4omni/omnicmmc/dto/AccessTokenDto.java
@@ -0,0 +1,12 @@
+package com.altf4omni.omnicmmc.dto;
+
+public class AccessTokenDto {
+    //Test//
+    private String token_type;
+
+
+    private int expires_in;
+
+
+    private String scope;
+}
--- src/main/java/com/altf4omni/omnicmmc/repository/UserRepository.java
+++ src/main/java/com/altf4omni/omnicmmc/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.altf4omni.omnicmmc.repository;
+import com.altf4omni.omnicmmc.entity.User;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+@Repository
+public interface UserRepository extends CrudRepository<com.altf4omni.omnicmmc.entity.User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.username = :username")
     public User getUserByUsername(@Param("username") String username);
