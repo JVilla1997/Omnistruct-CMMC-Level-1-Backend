9a2cfdc1d96006278399253158a898a608d01e2f
JVilla1997 <jorge.villavicencio.013@gmail.com>
2023-04-01 18:50:22 -0700

    changing security issue

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
 FROM eclipse-temurin:17-jdk-jammy
-EXPOSE 8080
-ARG JAR_FILE=target/OmniCMMC-0.0.1-SNAPSHOT.jar
-ADD ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+COPY ./target/OmniCMMC-0.0.1-SNAPSHOT.jar OmniCMMC-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java", "-jar", "OmniCMMC-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/altf4omni/omnicmmc/service/WebSecurityConfig.java
+++ src/main/java/com/altf4omni/omnicmmc/service/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.authorizeRequests().antMatchers("/**").permitAll()
                 .antMatchers("/").hasAnyAuthority("ADMIN", "CREATOR")
                 .antMatchers("/new").hasAnyAuthority("ADMIN", "CREATOR")
                 .antMatchers("/edit/**").hasAnyAuthority("ADMIN")
