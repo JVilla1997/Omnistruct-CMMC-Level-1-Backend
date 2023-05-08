6e809581e46233684bcb2b9d0d91a0c2d659f662
Rian Allen <rallen96@live.com>
2023-05-08 02:31:10 -0700

    cors hotfix

--- src/main/java/com/altf4omni/omnicmmc/service/WebSecurityConfig.java
+++ src/main/java/com/altf4omni/omnicmmc/service/WebSecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -25,7 +30,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.cors().and()
+                .authorizeRequests()
                 .antMatchers("/admin/**").authenticated()
                 .antMatchers(HttpMethod.PUT, "/password").authenticated()
                 .and()
